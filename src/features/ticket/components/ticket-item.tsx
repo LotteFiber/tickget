5eaefb9a57c8bf62b5b04b85d5c834b9dc1da127
NRP <43085120+LotteFiber@users.noreply.github.com>
2025-05-28 14:25:20 +0700

    feat: add ticket deletion, router caching, and dynamic tickets page (#4)

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -1,10 +1,12 @@
+"use client";
 import clsx from "clsx";
-import { LucideSquareArrowOutUpRight } from "lucide-react";
+import { LucideSquareArrowOutUpRight, LucideTrash } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Ticket } from "@/generated/prisma";
 import { ticketPath } from "@/paths";
+import { deleteTicket } from "../actions/delete-ticket";
 import { TICKET_ICONS } from "../constants";
 
 type TicketItemProps = {
@@ -15,9 +17,19 @@ type TicketItemProps = {
 const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
   const detailButton = (
     <Button className="outline" size="icon" asChild>
-      <Link href={ticketPath(ticket.id)}>
+      <Link prefetch href={ticketPath(ticket.id)}>
         <LucideSquareArrowOutUpRight className="h-4 w-4" />
       </Link>
+    </Button>
+  );
+
+  const handleDeleteTicket = async () => {
+    await deleteTicket(ticket.id);
+  };
+
+  const deleteButton = (
+    <Button className="outline" size="icon" onClick={handleDeleteTicket}>
+      <LucideTrash className="h-4 w-4" />
     </Button>
   );
 
@@ -46,9 +58,9 @@ const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
             </span>
           </CardContent>
         </Card>
-        {isDetail ? null : (
-          <div className="flex flex-col gap-y-1">{detailButton}</div>
-        )}
+        <div className="flex flex-col gap-y-1">
+          {isDetail ? deleteButton : detailButton}
+        </div>
       </div>
     </div>
   );
